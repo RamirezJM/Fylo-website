f8d2c71533bf8db4b270930651798d5feaf52875
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-08-30 21:54:06 -0400

    feat: add script for toggle dark mode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+const darkModeBtn = document.querySelector('.dark-mode')
+
+darkModeBtn.addEventListener('click', () =>{
+  const isDark = document.documentElement.classList.toggle('dark')
+  darkModeBtn.setAttribute('aria-pressed', String(isDark))
+  localStorage.theme = isDark ? 'dark': 'light'
+ } )
+
+
